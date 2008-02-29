f79caeda91df40d8f1a781e1637287d76c86ffc8
adrianofchaves <adrianofchaves@bf5c33f3-0144-0410-9aab-7dd680faec1a>
2008-02-29 15:30:56 +0000

    Inclusão do Script. Login com acesso ao banco.

--- source/Gelic/src/java/model/daos/Usuarios.java
+++ source/Gelic/src/java/model/daos/Usuarios.java
@@ -2,60 +2,43 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model.daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.naming.NamingException;
 
 /**
  * DAO responsável pelo acesso a tabela de usuário.
  * @author adriano
  */
 public class Usuarios {
+    public static final String sqlRecuperarLogin = 
+            "select PAPEL TIPO, LOGIN, SENHA from USUARIOS where LOGIN containing ?";
+    
+
     /**
      * Recupera um usuário do banco de dados. 
      * @param Login login do usuário.  Se for null a função retornará null.
      * @return model.beans.Usuario
      */
-    public static model.beans.Usuario recuperar( String login  ){
-        /**
-         * TODO: Aqui, o banco é o próprio programa.  Quando o banco estiver 
-         * implementado devemos programar um select. Que substituirá todos esses
-         * if...
-         */
-        if( login == null )
-            return null;
-        if( login.equalsIgnoreCase("ADMIN")){
-            model.beans.Usuario usuario = new model.beans.Usuario();
-            
-            usuario.setTipo(model.beans.Usuario.ADMINISTRADOR);
-            return usuario;
-        }
-        if( login.equalsIgnoreCase("ANALISTA")){
-            model.beans.Usuario usuario = new model.beans.Usuario();
-            
-            usuario.setTipo(model.beans.Usuario.ANALISTA);
-            return usuario;
-        }
-        if( login.equalsIgnoreCase("COMERCIAL")){
-            model.beans.Usuario usuario = new model.beans.Usuario();
-            
-            usuario.setTipo(model.beans.Usuario.COMERCIAL);
-            return usuario;
-        }
-        if( login.equalsIgnoreCase("FINANCEIRO")){
-            model.beans.Usuario usuario = new model.beans.Usuario();
-            
-            usuario.setTipo(model.beans.Usuario.FINANCEIRO);
-            return usuario;
-        }
-        if( login.equalsIgnoreCase("GERENTECOMERCIAL")){
-            model.beans.Usuario usuario = new model.beans.Usuario();
-            
-            usuario.setTipo(model.beans.Usuario.GERENTECOMERCIAL);
-            return usuario;
-        }
-            
-        return null;
-        
-    }
+    public static model.beans.Usuario recuperar(String login)
+            throws NamingException, SQLException {
 
+        Connection gelic = model.services.Conexao.getPool().getConnection();
+        PreparedStatement pstmt = gelic.prepareStatement(
+                sqlRecuperarLogin);
+        pstmt.setString(1, login);
+        ResultSet rs = pstmt.executeQuery();
+        if (!rs.next()) {
+            return null;
+        }
+        model.beans.Usuario usuario = new model.beans.Usuario();
+        usuario.setLogin(rs.getString("LOGIN"));
+        usuario.setSenha(rs.getString("SENHA"));
+        usuario.setTipo(rs.getInt("TIPO"));
+        return usuario;
+    }
 }
