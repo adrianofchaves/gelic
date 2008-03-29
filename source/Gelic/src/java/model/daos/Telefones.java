f912ef5b8b3f3d04a78e6e56cbbbd963743b4960
adrianofchaves <adrianofchaves@bf5c33f3-0144-0410-9aab-7dd680faec1a>
2008-03-29 13:08:55 +0000

    Cadastro de empresas - continuação.

--- source/Gelic/src/java/model/daos/Telefones.java
+++ source/Gelic/src/java/model/daos/Telefones.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import javax.naming.NamingException;
 
 /**
@@ -17,20 +18,51 @@ import javax.naming.NamingException;
  */
 public class Telefones {
 
+  static final String sqlRecuperarTelefonesEmpresas =
+          "select " +
+          " CNPJ, CONTATOS.NOME, TELEFONES.DDI, TELEFONES.DDD, " +
+          " TELEFONES.NUMERO, TELEFONES.RAMAL " +
+          "from " +
+          " EMPRESAS " +
+          " left outer join CONTATOS  on (CONTATOS.EMPRESA = EMPRESAS.CNPJ) " +
+          " inner join TELEFONES on (TELEFONES.ID = CONTATOS.TELEFONE) " +
+          "union " +
+          "select " +
+          " CNPJ, null, TELEFONES.DDI, TELEFONES.DDD, TELEFONES.NUMERO, " +
+          " TELEFONES.RAMAL " +
+          "from " +
+          " EMPRESAS " +
+          " inner join ENDERECOS on (EMPRESAS.ENDERECO = ENDERECOS.ID) " +
+          " inner join TELEFONES on (ENDERECOS.TELEFONE = TELEFONES.ID) ";
+
   public static void recuperar(ArrayList<model.beans.Empresa> empresas)
           throws SQLException, NamingException {
+
+    /* Para otimizar a busca, reorganiza as empresas em HashMaps */
+    HashMap<String, model.beans.Empresa> mapEmpresas;
+    mapEmpresas = new HashMap<String, model.beans.Empresa>();
+    HashMap<String, model.beans.Contato> mapContatos;
+    mapContatos = new HashMap<String, model.beans.Contato>();
+
     for (model.beans.Empresa empresa : empresas) {
-      recuperar(empresa);
+      mapEmpresas.put(empresa.getCnpj(), empresa);
+      for (model.beans.Contato contato : empresa.getContatos()) {
+        mapContatos.put(empresa.getCnpj() + contato.getNome(), contato);
+      }
     }
+    
+    Connection gelic = model.services.Conexao.getPool().getConnection();
+   // PreparedStatement pstmt = gelic.prepareStatement(
   }
-  static final String sqlRecuperaTelefoneEmpresa = "select DDI, DDD, TELEFONE, " +
+  static final String sqlRecuperaTelefoneEmpresa = 
+          "select DDI, DDD, TELEFONE, " +
           "RAMAL from  telefones  inner join enderecos on " +
           "(enderecos.telefone = telefones.id) inner join empresas on  " +
           "(empresas.endereco = enderecos.id) where empresas.cnpj=?";
 
   protected static void recuperar(model.beans.Empresa empresa)
           throws NamingException, SQLException {
-    if( empresa.getEndereco() == null){
+    if (empresa.getEndereco() == null) {
       return;
     }
     Connection gelic = model.services.Conexao.getPool().getConnection();
