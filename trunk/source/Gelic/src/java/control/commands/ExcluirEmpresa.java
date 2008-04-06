d8e8cf7dafd9a98956bc9602b1840a47fd22d725
adrianofchaves <adrianofchaves@bf5c33f3-0144-0410-9aab-7dd680faec1a>
2008-04-06 01:38:58 +0000

    Exclusão de Empresas - início

---
+++ trunk/source/Gelic/src/java/control/commands/ExcluirEmpresa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package control.commands;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Adriano
+ */
+public class ExcluirEmpresa implements Comando{
+
+  public String executar(HttpServletRequest req) throws ExcecaoComando {
+    try {
+      String cnpj = util.Request.getParameter(req, "cnpj");
+      if (cnpj == null) {
+        throw new ExcecaoComando("CNPJ inválido (nulo)");
+      }
+      model.beans.Empresa empresa = model.services.Empresas.recuperar(cnpj);
+      view.FormEmpresa frm = new view.FormEmpresa();
+      frm.setEmpresa(empresa);
+      frm.atualizaCampos();
+      frm.setInclusao(false);
+      frm.setExclusao(true);
+      frm.setTitulo("Excluindo empresa");
+      frm.setUfs(control.UFs.getUFs(req));
+      frm.setTiposLogradouros(control.Enderecos.getTiposLogradouros(req));
+      req.getSession().setAttribute("formEmpresa", frm);
+      return "/formEmpresa.jsp";
+    } catch (SQLException ex) {
+      Logger.getLogger(ExcluirEmpresa.class.getName()).log(
+              Level.SEVERE, null, ex);
+      throw new ExcecaoComando( ex.getMessage());
+    } catch (NamingException ex) {
+      Logger.getLogger(ExcluirEmpresa.class.getName()).log(
+              Level.SEVERE, null, ex);
+      throw new ExcecaoComando( ex.getMessage());
+    }
+  }
+
+}
