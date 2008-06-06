254b39e359ba8e7c7685ff162bab5615751471e5
adrianofchaves <adrianofchaves@bf5c33f3-0144-0410-9aab-7dd680faec1a>
2008-06-06 19:27:23 +0000

    Comando AlterarProposta

---
+++ source/Gelic/src/java/control/commands/AlterarProposta.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package control.commands;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Adriano
+ */
+public class AlterarProposta implements Comando{
+
+  public String executar(HttpServletRequest req) throws ExcecaoComando {
+    try {
+      String empresa = req.getParameter("empresa");
+      view.BrowserPropostas browser = (view.BrowserPropostas) req.getSession().
+            getAttribute(view.BrowserPropostas.NOME_ATRIBUTO_DEFAULT);
+      view.FormProposta form = new view.FormProposta();
+      req.getSession().setAttribute(view.FormProposta.NOME_ATRIBUTO_DEFAULT, 
+              form);
+      form.setOrigem(browser);
+      return form.preparaAlteracao(empresa);
+    } catch (NamingException ex) {
+      Logger.getLogger(AlterarProposta.class.getName()).
+              log(Level.SEVERE, null, ex);
+      throw new ExcecaoComando(ex.getMessage());
+    } catch (SQLException ex) {
+      Logger.getLogger(AlterarProposta.class.getName()).
+              log(Level.SEVERE, null, ex);
+      throw new ExcecaoComando(ex.getMessage());
+    }
+  }
+
+}
