120cb052d97f58d6bfd47280ce3a4c90fc1f6350
adrianofchaves <adrianofchaves@bf5c33f3-0144-0410-9aab-7dd680faec1a>
2008-06-11 15:58:04 +0000

    Corrigido Tipo de Licitação: não estava gravando.  Dava erro ao excluir.

--- trunk/source/Gelic/src/java/control/commands/GravarTipoLicitacao.java
+++ trunk/source/Gelic/src/java/control/commands/GravarTipoLicitacao.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author Adriano alterado por Paulo
  */
 public class GravarTipoLicitacao implements Comando {
-    
+
   static final String chaveForm = "formTipoLicitacao";
   static final String chaveBrowser = "browserTiposLicitacoes";
   static final String uriForm = "/formTipoLicitacao.jsp";
@@ -24,68 +24,68 @@ public class GravarTipoLicitacao implements Comando {
   static final String msgExclusao = "Tipo de Licitação excluído.";
   static final String msgAlteracao = "Tipo de Licitação alterado.";
 
-    public String executar(HttpServletRequest req) throws ExcecaoComando {
+  public String executar(HttpServletRequest req) throws ExcecaoComando {
 
-        try {
-            if (req.getParameter("cancelar") != null) {
-                return control.TipoLicitacao.preparaBrowser(req);
-            }
+    try {
+      if (req.getParameter("cancelar") != null) {
+        return control.TipoLicitacao.preparaBrowser(req);
+      }
 
-            view.FormTipoLicitacao frm = (view.FormTipoLicitacao) req.getSession().
-                    getAttribute(chaveForm);
-            
-            if (frm==null){
-                frm = new view.FormTipoLicitacao();
-            }                
-            
-            /* popula form */            
-            frm.setNomeTipoLicitacao(util.Request.getParameter(req,
-                    "nomeTipoLicitacao"));
-            frm.setSiglaTipoLicitacao(util.Request.getParameter(req,
-                    "siglaTipoLicitacao"));
-            
-            frm.valida();
-            
-            if( frm.temErros() ){
-                return uriForm;
-            }
-            
-            String mensagem = null;
-            
-            if(frm.isInclusao()){
-                model.services.TiposLicitacoes.incluir(
-                        frm.getNomeTipoLicitacao(), 
-                        frm.getSiglaTipoLicitacao());
-                mensagem = msgInclusao;
-            }
-            
-            if (frm.isAlteracao()){
-                model.services.TiposLicitacoes.alterar(
-                        frm.getTipoLicitacao().getNome(),
-                        frm.getNomeTipoLicitacao(),
-                        frm.getSiglaTipoLicitacao());
-                mensagem = msgAlteracao;
-            }
-            
-            if (frm.isExclusao()){
-                model.services.TiposLicitacoes.excluir(frm.getTipoLicitacao().getSigla());
-                mensagem = msgExclusao;            
-            }
-            
-            return executaBrowser(req, mensagem);
-            
-        } catch (NamingException ex) {
-            Logger.getLogger(GravarTipoLicitacao.class.getName()).log(
-                    Level.SEVERE, null, ex);
-            throw new ExcecaoComando(ex.getMessage());
-        } catch (SQLException ex) {
-            Logger.getLogger(GravarTipoLicitacao.class.getName()).log(
-                    Level.SEVERE, null, ex);
-            throw new ExcecaoComando(ex.getMessage());
-        }
+      view.FormTipoLicitacao frm = (view.FormTipoLicitacao) req.getSession().
+              getAttribute(chaveForm);
+
+      if (frm == null) {
+        frm = new view.FormTipoLicitacao();
+      }
+
+      /* popula form */
+      frm.setNomeTipoLicitacao(util.Request.getParameter(req,
+              "nomeTipoLicitacao"));
+      frm.setSiglaTipoLicitacao(util.Request.getParameter(req,
+              "siglaTipoLicitacao"));
+
+      frm.valida();
+
+      if (frm.temErros()) {
+        return uriForm;
+      }
+
+      String mensagem = null;
+
+      if (frm.isInclusao()) {
+        model.services.TiposLicitacoes.incluir(
+                frm.getNomeTipoLicitacao(),
+                frm.getSiglaTipoLicitacao());
+        mensagem = msgInclusao;
+      }
+
+      if (frm.isAlteracao()) {
+        model.services.TiposLicitacoes.alterar(
+                frm.getTipoLicitacao().getNome(),
+                frm.getNomeTipoLicitacao(),
+                frm.getSiglaTipoLicitacao());
+        mensagem = msgAlteracao;
+      }
+
+      if (frm.isExclusao()) {
+        model.services.TiposLicitacoes.excluir(frm.getTipoLicitacao().getSigla());
+        mensagem = msgExclusao;
+      }
+
+      return executaBrowser(req, mensagem);
+
+    } catch (NamingException ex) {
+      Logger.getLogger(GravarTipoLicitacao.class.getName()).log(
+              Level.SEVERE, null, ex);
+      throw new ExcecaoComando(ex.getMessage());
+    } catch (SQLException ex) {
+      Logger.getLogger(GravarTipoLicitacao.class.getName()).log(
+              Level.SEVERE, null, ex);
+      throw new ExcecaoComando(ex.getMessage());
     }
-    
-    private String executaBrowser(HttpServletRequest req, String mensagem) 
+  }
+
+  private String executaBrowser(HttpServletRequest req, String mensagem)
           throws NamingException, SQLException {
     view.BrowserTiposLicitacoes browser = new view.BrowserTiposLicitacoes(
             model.services.TiposLicitacoes.recuperar());
